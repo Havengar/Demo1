de428ad1c1063880b442eea23b0ccc4838a04985
Fabio Bittencourt <fabiobittencourt@msn.com>
2013-04-05 11:06:41 +0200

    Initial Commit

---
+++ demo1/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  demo1
+//
+//  Created by Havengar on 2013-04-05.
+//  Copyright (c) 2013 Fabulous. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+

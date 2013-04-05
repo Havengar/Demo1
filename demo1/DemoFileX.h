c49d01676ef9526e7df1312523f38cd323b2b535
Fabio Bittencourt <fabiobittencourt@msn.com>
2013-04-05 11:18:48 +0200

    Added file demoFileX

---
+++ demo1/DemoFileX.h
@@ -0,0 +1,13 @@
+//
+//  DemoFileX.h
+//  demo1
+//
+//  Created by Havengar on 2013-04-05.
+//  Copyright (c) 2013 Fabulous. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DemoFileX : NSObject
+
+@end
---
+++ demo1/DemoFileX.m
@@ -0,0 +1,13 @@
+//
+//  DemoFileX.m
+//  demo1
+//
+//  Created by Havengar on 2013-04-05.
+//  Copyright (c) 2013 Fabulous. All rights reserved.
+//
+
+#import "DemoFileX.h"
+
+@implementation DemoFileX
+
+@end
